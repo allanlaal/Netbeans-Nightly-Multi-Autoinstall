c3d708d52414f818c63a44230e7d704ef120b5bd
Luis Lobo Borobia <luislobo@gmail.com>
2012-03-29 09:17:29 -0300

    Update getNetbeansNightly.sh

--- getNetbeansNightly.sh
+++ getNetbeansNightly.sh
@@ -9,10 +9,10 @@ mkdir -p $NETBEANS_NIGHTLY_DIR
 
 echo "Downloading Netbeans Nightly Build..."
 cd $DOWN_DIR
-#lynx -dump http://bits.netbeans.org/download/trunk/nightly/latest/zip | grep http://bits.netbeans.org/download/trunk/nightly/latest/zip/netbeans | awk '{print $2}' | tail -1 | wget -i - --output-document=$DOWN_DIR/$NETBEANS_ZIP
+lynx -dump http://bits.netbeans.org/download/trunk/nightly/latest/zip | grep http://bits.netbeans.org/download/trunk/nightly/latest/zip/netbeans | awk '{print $2}' | tail -1 | wget -i - --output-document=$DOWN_DIR/$NETBEANS_ZIP
 
 echo "Unzipping Netbeans..."
-#unzip -uo -v $DOWN_DIR/$NETBEANS_ZIP -d $NETBEANS_NIGHTLY_DIR
+unzip -uo $DOWN_DIR/$NETBEANS_ZIP -d $NETBEANS_NIGHTLY_DIR
 
 echo "Building Netbeans..."
 export ANT_OPTS="-Xmx256m -XX:MaxPermSize=96m"
