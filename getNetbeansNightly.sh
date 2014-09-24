ff204b3ea03afd343280eda9ad5c0cd300ff4036
Allan Laal <allan@permanent.ee>
2014-09-25 01:38:13 +0300

    minor (no actual changes)

--- getNetbeansNightly.sh
+++ getNetbeansNightly.sh
@@ -15,23 +15,23 @@ then
 	source "$DIR/config.sh"
 
 
-echo "Setting up directories..."
-mkdir -p $DOWN_DIR
-mkdir -p $NETBEANS_NIGHTLY_DIR
+	echo "Setting up directories..."
+	mkdir -p $DOWN_DIR
+	mkdir -p $NETBEANS_NIGHTLY_DIR
 
-echo "Downloading Netbeans Nightly Build..."
-cd $DOWN_DIR
-lynx -dump http://bits.netbeans.org/download/trunk/nightly/latest/zip | grep http://bits.netbeans.org/download/trunk/nightly/latest/zip/netbeans | awk '{print $2}' | tail -1 | wget -i - --output-document=$DOWN_DIR/$NETBEANS_ZIP
+	echo "Downloading Netbeans Nightly Build..."
+	cd $DOWN_DIR
+	lynx -dump http://bits.netbeans.org/download/trunk/nightly/latest/zip | grep http://bits.netbeans.org/download/trunk/nightly/latest/zip/netbeans | awk '{print $2}' | tail -1 | wget -i - --output-document=$DOWN_DIR/$NETBEANS_ZIP
 
-echo "Unzipping Netbeans..."
-unzip -uo $DOWN_DIR/$NETBEANS_ZIP -d $NETBEANS_NIGHTLY_DIR
+	echo "Unzipping Netbeans..."
+	unzip -uo $DOWN_DIR/$NETBEANS_ZIP -d $NETBEANS_NIGHTLY_DIR
 
-echo "Building Netbeans..."
-export ANT_OPTS="-Xmx256m -XX:MaxPermSize=96m"
-cd $NETBEANS_NIGHTLY_DIR/nbbuild
-ant | tee > build.log
+	echo "Building Netbeans..."
+	export ANT_OPTS="-Xmx256m -XX:MaxPermSize=96m"
+	cd $NETBEANS_NIGHTLY_DIR/nbbuild
+	ant | tee > build.log
 
-ln -s ~/bin/nbdev $NETBEANS_NIGHTLY_DIR/nbbuild/netbeans/bin/netbeans
+	ln -s ~/bin/nbdev $NETBEANS_NIGHTLY_DIR/nbbuild/netbeans/bin/netbeans
 else
 	echo "copy the contents of config.example.sh to config.sh and modify the paths in that file as needed"
 fi
